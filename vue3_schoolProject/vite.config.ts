630de71d33cbe88f3eb69655786914ca89f8de07
TomoriNao <zjhzjhanpanman@gmail.com>
2023-05-16 21:15:36 +0800

    MPA_bug_fixs

--- vue3_schoolProject/vite.config.ts
+++ vue3_schoolProject/vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+        input: {
+            index: path.resolve(__dirname, 'index.html'),
+            admin: path.resolve(__dirname, 'admin.html'),
+        },
+    },
+}
 })
