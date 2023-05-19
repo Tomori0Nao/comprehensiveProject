6ae8314cd0f8ad41cefd669541e830da55f9f6bf
TomoriNao <zjhzjhanpanman@gmail.com>
2023-05-19 20:12:19 +0800

    working_with_home_page

--- vue3_schoolProject/src/router/index.ts
+++ vue3_schoolProject/src/router/index.ts
@@ -16,6 +16,26 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: () => import('../views/Home.vue')
+    },
+    {
+      path: '/VIP',
+      name: 'VIP',
+      component: () => import('../views/VIPView.vue')
+    },
+    {
+      path: '/Cart',
+      name: 'Cart',
+      component: () => import('../views/CartView.vue')
+    },
+    {
+      path: '/MyInfo',
+      name: 'MyInfo',
+      component: () => import('../views/MyinfoView.vue')
     }
   ]
 })
---
+++ vue3_schoolProject/src/views/CartView.vue
@@ -0,0 +1,11 @@
+
+import type { RouterLink } from 'vue-router';
+
+<template>
+    <span>CartView</span>
+    
+</template>
+  
+<script setup lang="ts">
+</script>
+  
